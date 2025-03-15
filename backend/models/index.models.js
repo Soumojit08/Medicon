eebf97191f67a211ba8cdb2b046311157f896880
Samiran Samanta <130337647+Samiran2004@users.noreply.github.com>
2025-03-15 20:04:01 +0530

    fix/Login for users, doctors and admins<Samiran2004>

--- backend/models/index.models.js
+++ backend/models/index.models.js
@@ -2,12 +2,14 @@ import Doctor from "./Doctors.model.js";
 import User from "./Users.model.js";
 import Appointment from "./Appointment.model.js";
 import Schedule from "./Schedule.model.js";
+import Admin from "./Admins.model.js";
 
 const Models = {
   UserModel: User,
   DoctorModel: Doctor,
   AppointmentModel: Appointment,
   ScheduleModel: Schedule,
+  AdminModel: Admin,
 };
 
 export default Models;
