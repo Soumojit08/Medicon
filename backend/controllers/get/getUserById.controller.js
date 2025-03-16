433a7d6898056140352b000974f118ab8a4e7bd6
Samiran2004 <samirans170@gmail.com>
2025-03-16 18:56:33 +0530

    fix/Get Doctor and User by id<Samiran2004>

--- backend/controllers/get/getUserById.controller.js
+++ backend/controllers/get/getUserById.controller.js
@@ -1,21 +1,28 @@
 import { getReasonPhrase, StatusCodes } from "http-status-codes";
 import Models from "../../models/index.models.js";
 
-const getUserByIdController = async(req, res)=>{
+const getUserByIdController = async (req, res) => {
     try {
         const id = req.params['id'];
-        if(!id){
+        if (!id) {
             return res.status(StatusCodes.BAD_REQUEST).json({
                 status: 'Failed',
                 message: "id is required!",
             });
         }
-        const User = await Models.UserModel.findById(id).select("-password");
-        return res.status(StatusCodes.OK).json({
-            status: 'OK',
-            message: `User with id: ${id}`,
-            data: User
-        });
+        try {
+            const User = await Models.UserModel.findById(id).select("-password");
+            return res.status(StatusCodes.OK).json({
+                status: 'OK',
+                message: `User with id: ${id}`,
+                data: User
+            });
+        } catch (error) {
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                status: 'Failed',
+                message: "Invalid Id!"
+            });
+        }
     } catch (error) {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             status: 'Failed',
