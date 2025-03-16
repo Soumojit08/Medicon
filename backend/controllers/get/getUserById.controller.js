cf8b2331a6c6f0f4da08213917e9bc2fb4d88c61
Samiran2004 <samirans170@gmail.com>
2025-03-16 18:12:54 +0530

    add/Get Users By Id<Samiran2004

---
+++ backend/controllers/get/getUserById.controller.js
@@ -0,0 +1,27 @@
+import { getReasonPhrase, StatusCodes } from "http-status-codes";
+import Models from "../../models/index.models.js";
+
+const getUserByIdController = async(req, res)=>{
+    try {
+        const id = req.params['id'];
+        if(!id){
+            return res.status(StatusCodes.BAD_REQUEST).json({
+                status: 'Failed',
+                message: "id is required!",
+            });
+        }
+        const User = await Models.UserModel.findById(id).select("-password");
+        return res.status(StatusCodes.OK).json({
+            status: 'OK',
+            message: `User with id: ${id}`,
+            data: User
+        });
+    } catch (error) {
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+            status: 'Failed',
+            message: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR)
+        });
+    }
+}
+
+export default getUserByIdController;
