453b7642397cd73e99ed642ab43223ffee9225a4
Samiran2004 <samirans170@gmail.com>
2025-03-30 07:04:06 +0530

    add/User component in swagger docs<Samiran2004>

--- backend/models/Users.model.js
+++ backend/models/Users.model.js
@@ -20,13 +20,9 @@ const UserSchema = new mongoose.Schema(
       required: true,
     },
     profilepic: {
-<<<<<<< HEAD
-        type: String,
-        default: "https://static.vecteezy.com/system/resources/previews/035/066/209/non_2x/user-avatar-male-illustration-design-free-png.png"
-    }
-}, { timestamps: true });
-=======
       type: String,
+      default:
+        "https://static.vecteezy.com/system/resources/previews/035/066/209/non_2x/user-avatar-male-illustration-design-free-png.png",
     },
     geoLocation: {
       type: {
@@ -49,8 +45,7 @@ const UserSchema = new mongoose.Schema(
 
 // Add geospatial index for location-based queries
 UserSchema.index({ geoLocation: "2dsphere" });
->>>>>>> 85ca1fd5be43ac107d69be98df0e25ce40f06d74
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 export default User;
